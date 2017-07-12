277c3e8c32eb7b2e0115a25d15c3f6a53efdbd53
User <User@DESK-GN2GJ32.lexicon.edu.local>
2017-07-12 15:24:32 +0200

    Juri

--- FlightReservation/src/Food.java
+++ FlightReservation/src/Food.java
@@ -1,4 +1,28 @@
 
 public class Food {
 
+	private String foodName;
+	private int foodPrice;
+	
+	public Food(String foodName, int foodPrice){
+		this.foodName = foodName;
+		this.foodPrice = foodPrice;
+		
+	}
+	
+	public String getFoodName() {
+		return foodName;
+	}
+	public void setFoodName(String foodName) {
+		this.foodName = foodName;
+	}
+	public int getFoodPrice() {
+		return foodPrice;
+	}
+	public void setFoodPrice(int foodPrice) {
+		this.foodPrice = foodPrice;
+	}
+	
+	
+	
 }
