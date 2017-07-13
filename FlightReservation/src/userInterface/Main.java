e3832a91f6b7e7494bb9bb7575e16830256e38ce
dianakhosravi <diana.khosravi.85@gmail.com>
2017-07-13 14:39:50 +0200

    Add main method

--- FlightReservation/src/userInterface/Main.java
+++ FlightReservation/src/userInterface/Main.java
@@ -1,5 +1,20 @@
 package userInterface;
 
+import java.io.IOException;
+
+import models.reservations.FlightReservationSystem;
+
 public class Main {
+
+	public static void main(String[] args) throws IOException {
+		
+		FlightReservationSystem system = new FlightReservationSystem();
+		
+		system.start();
+		
+		
+		
+
+	}
 
 }
