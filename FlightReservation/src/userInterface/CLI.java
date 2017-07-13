63ad5c0a0897e0771fb119927381019d0b69e430
dianakhosravi <diana.khosravi.85@gmail.com>
2017-07-13 14:35:17 +0200

    Add printMenu method

--- FlightReservation/src/userInterface/CLI.java
+++ FlightReservation/src/userInterface/CLI.java
@@ -1,7 +1,7 @@
 package userInterface;
 
 public class CLI {
-
+	
 	// 1. Create flight
 	// 2. Add aeroplane
 	// 3. New Customer
@@ -12,11 +12,28 @@ public class CLI {
 	// 8. Print all aeroplanes
 	// 9. Statistics
 	// 10. Economy
-
-	// switch()
-
-//	for(Food f : first.getFoodList()){
-//		System.out.println(f.getFoodName() + " " + f.getFoodPrice());
-//	}
-}
+	
+	
+		public void printMenu(){
+			
+			System.out.println("");
+			System.out.println("------------------------------------------------");
+			System.out.println("Menu");
+			System.out.println("------------------------------------------------");
+			System.out.println("1. Create flight");
+			System.out.println("2. Add aeroplane");
+			System.out.println("3. New Customer");
+			System.out.println("4. Buy Ticket");
+			System.out.println("5. Reserve Food");
+			System.out.println("6. Print all customers");
+			System.out.println("7. Print all flights");
+			System.out.println("8. Print all aeroplanes");
+			System.out.println("9. Statistics");
+			System.out.println("10. Economy");
+			System.out.println("11. Exit program");
+			System.out.println("-----------------------------------------------");
+			System.out.println("");
+		}
+		
+	}
 
